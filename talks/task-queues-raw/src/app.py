cd6cc06abbd2a437b7bf71ac3a3802f9b8758c6f
Artem Malyshev <proofit404@gmail.com>
2016-05-11 22:20:35 +0300

    Enable json serialization.

--- talks/task-queues-raw/src/app.py
+++ talks/task-queues-raw/src/app.py
@@ -2,21 +2,23 @@ import os
 
 from celery import Celery
 
+app = Celery(__name__)
+
 if os.getenv('USE_RABBITMQ'):
-    kwargs = {
-        'broker': 'amqp://pyconru:pyconru@localhost:5672/pyconru',
-        'backend': 'redis://localhost:6379/0'
-    }
+    app.conf.update(
+        BROKER_URL='amqp://pyconru:pyconru@localhost:5672/pyconru',
+        CELERY_RESULT_BACKEND='redis://localhost:6379/0')
+elif os.getenv('USE_REDIS'):
+    app.conf.update(
+        BROKER_URL='redis://localhost:6380/0',
+        CELERY_RESULT_BACKEND='redis://localhost:6381/0')
+else:
+    raise Exception('Bad')
 
-if os.getenv('USE_REDIS'):
-    kwargs = {
-        'broker': 'redis://localhost:6380/0',
-        'backend': 'redis://localhost:6381/0',
-    }
-
-app = Celery(__name__, **kwargs)
-
-del kwargs
+app.conf.update(
+    CELERY_ACCEPT_CONTENT=['json'],
+    CELERY_TASK_SERIALIZER='json',
+    CELERY_RESULT_SERIALIZER='json')
 
 
 @app.task
