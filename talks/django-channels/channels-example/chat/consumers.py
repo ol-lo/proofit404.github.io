2cbdea6d91d871c02761eebfa4e28970870de90d
Artem Malyshev <proofit404@gmail.com>
2016-10-07 00:27:43 +0300

    Use channel session.

--- talks/django-channels/channels-example/chat/consumers.py
+++ talks/django-channels/channels-example/chat/consumers.py
@@ -1,18 +1,26 @@
 from channels import Group
+from channels.sessions import channel_session
 
 
+@channel_session
 def ws_connect(message):
 
-    Group('chat').add(message.reply_channel)
+    room = message.content['path'].strip('/')
+    message.channel_session['room'] = room
+    Group('chat-%s' % room).add(message.reply_channel)
 
 
+@channel_session
 def ws_disconnect(message):
 
-    Group('chat').discard(message.reply_channel)
+    Group('chat-%s' % message.channel_session['room']).discard(
+        message.reply_channel,
+    )
 
 
+@channel_session
 def ws_message(message):
 
-    Group('chat').send({
+    Group('chat-%s' % message.channel_session['room']).send({
         'text': '[user] %s' % message.content['text'],
     })
