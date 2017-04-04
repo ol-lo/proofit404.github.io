01c6d0f9ec48a941522054566ef9e601c79707fb
Artem Malyshev <proofit404@gmail.com>
2017-04-04 20:17:59 +0300

    Define graphene schema.

--- talks/graphql-is-comming/example/taskmanager/schema.py
+++ talks/graphql-is-comming/example/taskmanager/schema.py
@@ -1,9 +1,44 @@
 import graphene
+from graphene_django import DjangoObjectType
+
+from .models import Comment as CommentModel
+from .models import Employee as EmployeeModel
+from .models import Status as StatusModel
+from .models import Task as TaskModel
+
+
+class Comment(DjangoObjectType):
+
+    class Meta:
+
+        model = CommentModel
+
+
+class Employee(DjangoObjectType):
+
+    class Meta:
+
+        model = EmployeeModel
+        exclude_fields = ('user', 'password', 'status')
+
+
+class Status(DjangoObjectType):
+
+    class Meta:
+
+        model = StatusModel
+
+
+class Task(DjangoObjectType):
+
+    class Meta:
+
+        model = TaskModel
 
 
 class Query(graphene.ObjectType):
 
-    hello = graphene.String()
+    employees = graphene.List(Employee)
 
 
 schema = graphene.Schema(query=Query)
