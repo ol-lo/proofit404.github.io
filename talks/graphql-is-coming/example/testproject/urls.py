835ceb063c5d8242af644d7699445cab0e9cf29f
Artem Malyshev <proofit404@gmail.com>
2017-06-16 19:40:55 +0300

    Add home redirect url.

--- talks/graphql-is-coming/example/testproject/urls.py
+++ talks/graphql-is-coming/example/testproject/urls.py
@@ -1,13 +1,17 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from graphene_django.views import GraphQLView
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/login/', admin.site.login),
-    url(r'^graphql/', GraphQLView.as_view(graphiql=True)),
     url(
         r'^taskmanager/',
         include('taskmanager.urls', namespace='taskmanager'),
+    ),
+    url(
+        r'^$',
+        RedirectView.as_view(pattern_name='taskmanager:graphql'),
+        name='home',
     ),
 ]
